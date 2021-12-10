bcad81193a34ef1c77683919f309382086fa5f77
Andrew Pruski <andrew.pruski@channeladvisor.com>
2021-12-10 15:43:01 +0000

    added flux demo

---
+++ demos/7. Automating Releases with Flux.ps1
@@ -0,0 +1,142 @@
+###################################################################################################
+###################################################################################################
+#
+# https://github.com/dbafromthecold/SQLServerKubernetesHelm
+# 
+# Using flux to automate deployments to Kubernetes
+# https://fluxcd.io/docs/guides/helmreleases/
+#  
+# Andrew Pruski
+# 
+###################################################################################################
+###################################################################################################
+
+
+
+# confirm pre-requisites
+flux check --pre
+
+
+
+# boot strap the cluster
+flux bootstrap git \
+  --url=https://github.com/dbafromthecold/DemoHelmRepo \
+  --username=dbafromthecold \
+  --password=XXXXXXXXXXXXXXXXXXXXX \
+  --token-auth=true \
+  --path=clusters/raspberrypi
+
+
+
+# create Helm Repository resource
+echo 'apiVersion: source.toolkit.fluxcd.io/v1beta1
+kind: HelmRepository
+metadata:
+  name: helmrepo
+  namespace: flux-system
+spec:
+  interval: 1m
+  url: https://raw.githubusercontent.com/dbafromthecold/DemoHelmRepo/main' > helmsource.yaml
+kubectl apply -f helmsource.yaml
+
+
+
+# view resource
+kubectl get HelmRepository -n flux-system
+
+
+
+
+# create Helm Release resource
+echo 'apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: azure-sql-edge
+  namespace: default
+spec:
+  interval: 1m
+  chart:
+    spec:
+      chart: azure-sql-edge
+      version: ''0.0.0 - 2.0.0''
+      sourceRef:
+        kind: HelmRepository
+        name: helmrepo
+        namespace: flux-system
+      interval: 1m
+  values:
+    replicaCount: 1' > helmrelease.yaml
+kubectl apply -f helmrelease.yaml
+
+
+
+# copy chart into repository
+cp /mnt/c/git/dbafromthecold/SQLServerKubernetesHelm/azure-sql-edge-1.0.0.tgz .
+
+
+
+# index repository
+helm repo index .
+
+
+
+# push chart to repo
+git add .
+git commit -m "updated repo"
+git push
+
+
+
+# confirm release deployed
+helm list
+
+
+
+# view kubernetes objects
+kubectl get all
+
+
+
+# connect to SQL
+IpAddress=$(kubectl get service sqledge-deployment --no-headers -o custom-columns=":status.loadBalancer.ingress[*].ip")
+mssql-cli -S $IpAddress -U sa -P Testing1122 -Q "SELECT @@VERSION AS [Version];"
+
+
+
+# copy new chart into repository
+cp /mnt/c/git/dbafromthecold/SQLServerKubernetesHelm/azure-sql-edge-1.1.0.tgz .
+
+
+
+# re-index repository
+helm repo index .
+
+
+
+# push new chart to repository
+git status
+git add .
+git commit -m "added new chart"
+git push
+
+
+
+# list releases
+helm list
+
+
+
+# view kubernetes objects
+kubectl get all
+
+
+
+# connect to SQL
+IpAddress=$(kubectl get service sqledge-deployment --no-headers -o custom-columns=":status.loadBalancer.ingress[*].ip")
+mssql-cli -S $IpAddress -U sa -P Testing1122 -Q "SELECT @@VERSION AS [Version];"
+
+
+
+# clean up
+kubectl delete helmrelease azure-sql-edge
+
