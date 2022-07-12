210406275966beadfe16e08081be2f513599358f
Andrew Pruski <dbafromthecold@gmail.com>
2022-07-12 22:19:40 +0100

    updated demo 6

--- SqlServer/demos/6. Creating a Remote Helm Repository.ps1
+++ SqlServer/demos/6. Creating a Remote Helm Repository.ps1
@@ -102,7 +102,7 @@ helm get manifest testchart | code -
 
 
 
-# connect to azure sql edge
+# connect to sql server
 IpAddress=$(kubectl get service -n sqlserver --no-headers -o custom-columns=":status.loadBalancer.ingress[*].ip") && echo $IpAddress
 mssql-cli -S $IpAddress -U sa -P Testing1122 -Q "SELECT @@VERSION AS [Version];"
 
